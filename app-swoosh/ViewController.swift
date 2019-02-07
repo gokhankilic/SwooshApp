2e0568bbfc4639f40e1d099cde448d3a1937dc05
Gökhan Kılıç <gokhankilic@Gokhans-MacBook-Air.local>
2019-02-07 10:44:14 +0300

    Initial Commit

---
+++ app-swoosh/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  app-swoosh
+//
+//  Created by Gökhan Kılıç on 7.02.2019.
+//  Copyright © 2019 Gökhan Kılıç. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
